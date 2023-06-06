0525805ecb968351b0e20cbeb5629bca0f60ef59
euna <wse0419@ewhain.net>
2023-06-06 20:20:15 +0900

    개찰구까지 1차 Goal
    낙하물 남아있음

--- Project/Assets/Scenes/Scripts/GroundShaker.cs
+++ Project/Assets/Scenes/Scripts/GroundShaker.cs
@@ -7,32 +7,38 @@ public class GroundShaker : MonoBehaviour
     //How strong is the earthquake?
     public static float magnitude = 4; //Not the same magnitude people talk about in an actual earthquakes
     public float slowDownFactor = 0.1f;
+    public GameObject Target;
 
     private Vector3 originalPosition;
 
 
-
     void Start()
     {
-        originalPosition = transform.position;
+        Debug.Log("start position");
+        originalPosition = Target.transform.localPosition;
+        Debug.Log(originalPosition);
+
+        magnitude = Random.Range(1, 8);
     }
 
     void FixedUpdate()
     {
         Debug.Log(magnitude);
 
-        Vector2 randomPos = Random.insideUnitCircle * magnitude * 40;
+        Vector2 randomPos = Random.insideUnitCircle * magnitude;
 
-        float randomY = Random.Range(-1f, 1f) * magnitude * 40;
+        float randomY = Random.Range(-1f, 1f) * magnitude;
 
-        float randomX = Mathf.Lerp(transform.position.x, randomPos.x, Time.deltaTime * slowDownFactor);
-        float randomZ = Mathf.Lerp(transform.position.z, randomPos.y, Time.deltaTime * slowDownFactor);
+        float randomX = Mathf.Lerp(transform.localPosition.x, randomPos.x, Time.deltaTime * slowDownFactor); ;
+        float randomZ = Mathf.Lerp(transform.localPosition.z, randomPos.y, Time.deltaTime * slowDownFactor);
 
-        randomY = Mathf.Lerp(transform.position.y, randomY, Time.deltaTime * slowDownFactor * 0.1f);
+        randomY = Mathf.Lerp(transform.localPosition.y, randomY, Time.deltaTime * slowDownFactor * 0.1f);
 
         Vector3 moveVec = new Vector3(randomX, randomY, randomZ);
 
-        transform.position = originalPosition + moveVec;
-        transform.rotation = Quaternion.Euler(moveVec);
+        transform.localPosition = originalPosition + moveVec;
+        transform.localRotation = Quaternion.Euler(moveVec);
+
+        Debug.Log(transform.localPosition);
     }
 }
