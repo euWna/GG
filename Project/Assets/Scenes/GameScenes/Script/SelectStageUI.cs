81b8ca2df8e36f7a31ba8c09eda591df8d7e2d8a
박현진 <r435675@gmail.com>
2023-06-08 20:25:05 +0900

    로비 수정

--- Project/Assets/Scenes/GameScenes/Script/SelectStageUI.cs
+++ Project/Assets/Scenes/GameScenes/Script/SelectStageUI.cs
@@ -9,6 +9,8 @@ public class SelectStageUI : MonoBehaviour
 {
 
     public TextMeshProUGUI SelectedStage;
+    public Image mapImage;
+    public Sprite subway, apart;
     
     // Start is called before the first frame update
 
@@ -34,8 +36,15 @@ public class SelectStageUI : MonoBehaviour
         m_iStageIndex = (m_iStageIndex + 1) % 2;
         switch(m_iStageIndex)
         {
-            case 0: SelectedStage.text = "Subway"; break;
-            case 1: SelectedStage.text = "Apartment"; break;
+            case 0:
+                SelectedStage.text = "Subway";
+                mapImage.sprite = subway;
+
+                break;
+            case 1:
+                SelectedStage.text = "Apartment";
+                mapImage.sprite = apart;
+                break;
         }
         //SelectedStage.text = sz_SelectedStage;
     }
